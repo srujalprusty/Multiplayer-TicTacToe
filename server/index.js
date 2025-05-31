fe2075fce6fca822fff20ac3dc82e573edf0dd04
SRUJAL PRUSTY <s.srujal0612@gmail.com>
2025-05-31 11:12:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ var io = require("socket.io")(server);
 app.use(express.json());
 
 const DB =
-  "mongodb+srv://rivaan:test123@cluster0.rmhtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  " ";
 
 io.on("connection", (socket) => {
   console.log("connected!");
